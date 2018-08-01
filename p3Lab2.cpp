50bf06f8d947d3a673ba2c3b5d206fd1fe7a9070
LMontoya19 <leomontoya19@hotmail.com>
2018-07-31 18:55:51 -0600

    Menu

--- p3Lab2.cpp
+++ p3Lab2.cpp
@@ -4,6 +4,35 @@ using std::cin;
 using std::endl;
 
 int main(){
-	cout<<"hola";
+	char resp='s';
+	while (resp=='s'){
+		cout<<"***MENU***"<<endl;
+		cout<<"a)Ejercicio 1"<<endl;
+		cout<<"b)Ejercicio 2"<<endl;
+		cout<<"c)Ejercicio 3"<<endl;
+		cout<<"d)Salir"<<endl;
+		cout<<"Ingrese su opcion";
+		char opcion;
+		cin >> opcion;
+		switch(opcion) {
+   			case 'a'  :
+      				cout<<"1"<<endl;
+      				break;
+   			case 'b'  :
+      				cout<<"2"<<endl;
+      				break;
+			case 'c'  :
+				cout<<"3"<<endl;
+				break;
+			case 'd'  :
+				cout<<"4"<<endl;
+				resp='n';
+				break;
+   			default : 
+      			cout<<"opcion no valida"<<endl;
+		}
+	}
+
+
 	return 0;
 }
